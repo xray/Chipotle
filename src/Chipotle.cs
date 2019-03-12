35a2489ea63a7505320051f636008b19ba4fa0e0
xray <creator@darkmattercreations.com>
2019-03-11 21:21:09 -0500

    Extracted Chipotle from Chorizo to its own solution.

---
+++ src/Chipotle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace xray.Chipotle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = 5000;
+            var server = new Chorizo.Chorizo(port);
+            Console.WriteLine($"Starting to listen on port {port}");
+            server.Start();
+        }
+    }
+}
---
+++ tests/ChipotleTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace xray.Chipotle.Tests
+{
+    public class ChipotleTest
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
