efc451e63d497dcf28b35bf0bb8414a3bd18a89d
xray <creator@darkmattercreations.com>
2019-03-19 13:51:57 -0500

    Now implements Chorizo to pass acceptance tests.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Chipotle (C#/.NET)
+C# Version 7.2 | .NET Version 2.2.0
+  
+## Requirements
+* .NET SDK 2.2  
+  
+## Usage
+* In terminal from the project folder navigate to the `src` folder.
+* Run `dotnet run`
+* If you have access to the acceptance test suite you can run them against this server
+  
+## Tests
+How to run the tests:  
+* Run `dotnet test`
--- src/Chipotle.cs
+++ src/Chipotle.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Chorizo.HTTP.Exchange;
+using Chorizo.HTTP.ReqProcessor;
 
 namespace xray.Chipotle
 {
@@ -7,7 +9,49 @@ namespace xray.Chipotle
         static void Main(string[] args)
         {
             var port = 5000;
-            var server = new Chorizo.Chorizo(port);
+            var server = new Chorizo.App(port, routes: new Routes()
+                .Get("/simple_get", req =>
+                {
+                    return new Response("HTTP/1.1", 200, "OK")
+                        .AddHeader("Server", "Chorizo");
+                })
+                .Post("/echo_body", req =>
+                {
+                    return new Response("HTTP/1.1", 200, "OK", req.Body)
+                        .AddHeader("Server", "Chorizo");
+                })
+                .Get("/method_options", req =>
+                {
+                    return new Response("HTTP/1.1", 200, "OK")
+                        .AddHeader("Server", "Chprizo");
+                })
+                .Get("/method_options2", req =>
+                {
+                    return new Response("HTTP/1.1", 200, "OK")
+                        .AddHeader("Server", "Chorizo");
+                })
+                .Put("/method_options2", req =>
+                {
+                    return new Response("HTTP/1.1", 200, "OK")
+                        .AddHeader("Server", "Chorizo");
+                })
+                .Post("/method_options2", req =>
+                {
+                    return new Response("HTTP/1.1", 200, "OK")
+                        .AddHeader("Server", "Chorizo");
+                })
+                .Get("/redirect", req =>
+                {
+                    return new Response("HTTP/1.1", 301, "Moved Permanently")
+                        .AddHeader("Location", "http://localhost:5000/simple_get");
+                })
+                .Head("/get_with_body", req =>
+                {
+                    return new Response("HTTP/1.1", 200, "Method Not Allowed")
+                        .AddHeader("Server", "Chorizo");
+                })
+            );
+
             Console.WriteLine($"Starting to listen on port {port}");
             server.Start();
         }
